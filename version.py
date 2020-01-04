17b235dcad6f52c305e808ad65ca27678b103db9
Frédéric MOHIER <frederic.mohier@gmail.com>
2020-01-04 09:40:16 +0100

    Set version as 1.1

--- version.py
+++ version.py
@@ -17,7 +17,7 @@ __pkg_name__ = u"alignak_webui_graphite"
 __module_types__ = u"UI"
 
 # Application manifest
-__version__ = u"1.0.0"
+__version__ = u"1.1.0"
 __author__ = u"Frédéric Mohier"
 __author_email__ = u"frederic.mohier@alignak.net"
 __copyright__ = u"(c) 2015-2017 - %s" % __author__
